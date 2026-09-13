157e03743627a4e64fc0bbc73cdb4300d211e8c9
aakvatech <35020381+aakvatech@users.noreply.github.com>
2026-09-13 23:58:25 +0300

    test: cover member payments import

--- non_profit/non_profit/doctype/member/test_member.py
+++ non_profit/non_profit/doctype/member/test_member.py
@@ -1,8 +1,11 @@
 # Copyright (c) 2017, Frappe Technologies Pvt. Ltd. and Contributors
 # See license.txt
 
+import importlib
 import unittest
 
 
 class TestMember(unittest.TestCase):
-	pass
+	def test_member_module_imports_with_payments_dependency(self):
+		module = importlib.import_module("non_profit.non_profit.doctype.member.member")
+		self.assertTrue(hasattr(module, "get_payment_gateway_controller"))
